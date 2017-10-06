00fdd9fa50712722e82da4b19d1a5bf98c54f370
juanbella <jbellasantos1@gmail.com>
2017-10-06 09:49:49 +0200

    codigo

--- src/tabla.py
+++ src/tabla.py
@@ -1,15 +1,14 @@
 
 
 def sumar(a,b):
+    """
+    Suma los dos número pasados como argumento
 
-"""
-Suma los dos número pasados como argumento
+    :param a: fichero con los datos a leer
+    :param b: fichero en el que escribir los datos
+    :return: a + b
 
-:param a: fichero con los datos a leer
-:param b: fichero en el que escribir los datos
-:return: a + b
-    
-"""
-	return a + b
+    """
+    return a + b
 
 
