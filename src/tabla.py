3cba1d957ffc24c5d7eaa657691d39babb3b0b53
juanbella <jbellasantos1@gmail.com>
2018-01-23 11:37:27 +0100

    codigo

--- src/tabla.py
+++ src/tabla.py
@@ -1,16 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def sumar(a,b):
+def sumar_epydoc(a,b):
     """
-    Suma los dos número pasados como argumento
+    Suma los dos números pasados como argumento.
+    
+    Realiza la suma de dos números y devuelve el resultado.
 
-    :param a: fichero con los datos a leer
-    :param b: fichero en el que escribir los datos
+    @param a: primer número a sumar
+    @param b: segundo número a sumar
+    @return: a + b
+
+    """
+    return a + b
+    
+def sumar_rst(a,b):
+    """
+    Suma los dos números pasados como argumento.
+    
+    Realiza la suma de dos números y devuelve el resultado.
+
+    :param a: primer número a sumar
+    :param b: segundo número a sumar
     :return: a + b
 
     """
     return a + b
 
+def sumar_numpy(a,b):
+    """
+    Suma los dos números pasados como argumento.
+    
+    Realiza la suma de dos números y devuelve el resultado.
+    
+    Parameters
+    ----------
+    a : int or float
+        primer número a sumar
+    b : int or float
+        segundo número a sumar
+
+    Returns
+    -------
+    int or float
+        a + b
+
+    """
+    return a + b
+
+def sumar_google(a,b):
+    """
+    Suma los dos números pasados como argumento.
+    
+    Realiza la suma de dos números y devuelve el resultado.
+
+    Args:
+        a: fichero con los datos a leer
+        b: segundo número a sumar
+
+    Returns:
+        a + b
+
+    """
+    return a + b
+
 if __name__ == '__main__':
-    print(sumar(3,4))
+    print(sumar_rst(3,4))
